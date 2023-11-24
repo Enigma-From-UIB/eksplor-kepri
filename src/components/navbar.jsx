efa282adaf442c58517895cbbe11a5ba0eb6fa38
snowyish <90228920+snowyish@users.noreply.github.com>
2023-11-24 16:09:09 +0700

    Update navbar.jsx
    still need to center the navbar but keeping the 3 dot on right side plus make the desktop navbar responsive

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Logo from "../files/logo-kepri-white.svg";
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai';
 
@@ -7,31 +7,38 @@ import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai';
 */}
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false);
+
+    const handleNav = () => {
+        setNav(!nav);
+    };
+
     return (
-        <div className='text-white text-2xl mt-10 py-5 max-w-[1240-px] flex items-center justify-center'>
-            <ul className="flex mx-auto relative border-b">
-                <li className="p-8 border-b" onClick={event =>  window.location.href='/home.jsx'}>HOME</li>
-                <li className="p-8">PULAU</li>
-                <li className="p-8">BUDAYA</li>
-                <img src={Logo} className="relative" alt="logo" />
-                <li className="p-8">KULINER</li>
-                <li className="p-8">WISATA</li>
-                <li className="p-8">GAME</li>        
-                
-            </ul>
-            <div>
-                <AiOutlineMenu size={20} />
-            </div>
-            <div className="hidden fixed left-0 top-0 h-full w--[60%] border-r border-r-gray-900">
-            <img src={Logo} className="relative" alt="logo" />
-                <ul className="pt-12">
-                    
-                    <li className="p-8" onClick={event =>  window.location.href='/home.jsx'}>HOME</li>
+        <div className='text-white text-2xl mt-10 py-5 max-w-[1240-px] items-center justify-center flex md:justify-between  '>
+            <div className="flex items-center justify-center">
+                <ul className="hidden md:flex mx-auto relative border-b items-center">
+                    <li className="p-8 border-b" onClick={event =>  window.location.href='/home.jsx'}>HOME</li>
                     <li className="p-8">PULAU</li>
                     <li className="p-8">BUDAYA</li>
+                    <img src={Logo} className="relative" alt="logo" />
                     <li className="p-8">KULINER</li>
                     <li className="p-8">WISATA</li>
-                    <li className="p-8">GAME</li>  
+                    <li className="p-8">GAME</li>        
+                </ul>
+            </div>
+            <div onClick={handleNav} className="block md:hidden flex items-center justify-end">
+                {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+            </div>
+            <div className={!nav ? 'fixed left-0 top-0 h-full w-[60%] border-r bg-[#AAC8A2] bg-opacity-90 ease-in-out duration-500' : 'fixed left-[-100%] ease-in-out duration-500'}>
+                <img src={Logo} className="relative" alt="logo" />
+                <ul className="pt-12">
+                    
+                    <li className="p-8 border-b " onClick={event =>  window.location.href='/home.jsx'}>HOME</li>
+                    <li className="p-8 border-b ">PULAU</li>
+                    <li className="p-8 border-b ">BUDAYA</li>
+                    <li className="p-8 border-b ">KULINER</li>
+                    <li className="p-8 border-b ">WISATA</li>
+                    <li className="p-8 border-b ">GAME</li>  
                 </ul>
             </div>
         </div>
