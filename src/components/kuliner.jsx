01be4e9bdd5df916631355eafb827128e1a7e0d2
ZAfifa15 <147030759+ZAfifa15@users.noreply.github.com>
2023-11-28 20:46:49 +0700

    okee

--- src/components/kuliner.jsx
+++ src/components/kuliner.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import Luti from "../files/Luti gendang.jpg";
+
 
 function kuliner() {
   return (
     <div className=''>
         <h1 className='text-center text-4xl font-extrabold text-white'>
             Silahkan yang bagiannya kuliner, mulai coding di file ini :D <br/>
-            HAPPY CODING! fsddgffd
+            HAPPY CODING! bismillah bisa ya allah 
         </h1>
+        <img src={Luti} alt="Luti Gendang"/>
     </div>
   )
 }
