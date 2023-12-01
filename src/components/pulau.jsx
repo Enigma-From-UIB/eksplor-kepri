0d582201c8294fd6933e934c7d66e76029345080
StyvenZhuo <151433246+StyvenZhuo@users.noreply.github.com>
2023-12-01 21:42:13 +0700

    masih button

--- src/components/pulau.jsx
+++ src/components/pulau.jsx
@@ -3,57 +3,41 @@ import React from 'react'
 function pulau() {
   return (
     <div className=''>
-        <h1 className='text-center text-4xl font-extrabold text-white'>
+        <div className='text-center text-8xl font-extrabold text-white'>
         <div class="text-left">Natuna</div>
-        </h1>
-        <button
-    type="button"
-    class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text- shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-    data-te-ripple-init
-    data-te-ripple-color="light">
-    View
-  </button>
+        </div>
+    <button class="text-white rounded-full border-2 text-xs font-medium uppercase leading-normal text border-white text-lg ">VIEW</button>
 
-    <div class="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/4 container flex justify-end m-10 text-center container flex justify-end m-10 text-center ">
-  <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50">
+    <div class="basis-1/3 hover:basis-1/2">
+    <div class="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/6 container flex justify-end m-10 text-center container flex justify-end m-10 text-center ">
+  <div class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50">
     ANAMBAS
-  </h5>
-  <button
-    type="button"
-    class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text- shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-    data-te-ripple-init
-    data-te-ripple-color="light">
-    View
+  </div>
+  <button class="text-green rounded-full border-2 text-xs font-medium uppercase leading-normal text border-lime-500 ">VIEW
   </button>
-</div>
+  </div>
 
-<div class="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/4 container flex justify-end m-10 text-center">
-  <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50">
+<div class="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/6 container flex justify-end m-10 text-center">
+  <div class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50">
     LINGGA
-  </h5>
-  <button
-    type="button"
-    class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text- shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-    data-te-ripple-init
-    data-te-ripple-color="light">
-    View
-  </button>
+  </div>
+  <button class="text-green rounded-full border-2 text-xs font-medium uppercase leading-normal text border-lime-500 ">VIEW</button>
 </div> 
 
-<div class="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/4 container flex justify-end m-10 text-center">
-  <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50">
+<div class="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/6 container flex justify-end m-10 text-center">
+  <div class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50">
     KARIMUN
-  </h5>
-  <button
-    type="button"
-    class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text- shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-    data-te-ripple-init
-    data-te-ripple-color="light">
-    View
-  </button>
+  </div>
+  <button class="text-green rounded-full border-2 text-xs font-medium uppercase leading-normal text border-lime-500 ">VIEW</button>
 </div>
+<div class="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/6 container flex justify-end m-10 text-center">
+  <div class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50">
+    BATAM
+  </div>
+  <button class="text-green rounded-full border-2 text-xs font-medium uppercase leading-normal text border-lime-500 ">VIEW</button>
+</div> </div>
     </div>
-    
+
   )
 }
 
