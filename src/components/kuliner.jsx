00e1d300e25719db3ec81123155fd2f6ac798df2
ZAfifa15 <147030759+ZAfifa15@users.noreply.github.com>
2023-12-01 12:34:10 +0700

    Update kuliner.jsx

--- src/components/kuliner.jsx
+++ src/components/kuliner.jsx
@@ -7,7 +7,6 @@ function kuliner() {
     <div className=''>
         <h1 className='text-center text-4xl font-extrabold text-white'>
             Silahkan yang bagiannya kuliner, mulai coding di file ini :D <br/>
-            HAPPY CODING! bismillah bisa ya allah 
         </h1>
         <img src={Luti} alt="Luti Gendang"/>
     </div>
