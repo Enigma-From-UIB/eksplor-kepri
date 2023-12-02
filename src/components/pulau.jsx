95e52068f8551bbbb5f65d4a50c76186fcb9b907
StyvenZhuo <151433246+StyvenZhuo@users.noreply.github.com>
2023-12-02 21:50:27 +0700

    Update pulau.jsx

--- src/components/pulau.jsx
+++ src/components/pulau.jsx
@@ -35,6 +35,13 @@ function pulau() {
     BATAM
   </div>
   <button class="text-green rounded-full border-2 text-xs font-medium uppercase leading-normal text border-lime-500 ">VIEW</button>
+
+  <div class="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/6 container flex justify-end m-10 text-center">
+  <div class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50">
+    BINTAN
+  </div>
+  <button class="text-green rounded-full border-2 text-xs font-medium uppercase leading-normal text border-lime-500 ">VIEW</button>
+</div> 
 </div> </div>
     </div>
 
