bd00bc3e63dfc634961edf3609f13bf44d973213
StyvenZhuo <151433246+StyvenZhuo@users.noreply.github.com>
2023-12-03 17:16:35 +0700

    Update pulau.jsx

--- src/components/pulau.jsx
+++ src/components/pulau.jsx
@@ -4,7 +4,7 @@ function Pulau() {
   return (
     <div className='flex'>
       <li>
-        <h1 className='text-7xl font-serif font-bold text-white ml-20 text-left'>Natuna</h1>
+        <h1 className='text-7xl font-serif font-bold text-white ml-20 text-left mt-40'>Natuna</h1>
         <button
           type="button"
           className="mt-4 ml-20 inline-block bg-primary bg-transparent hover:bg-blue-500 text-white font-semibold hover:text-white py-2 px-4 border border-white hover:border-transparent rounded-full"
@@ -15,7 +15,7 @@ function Pulau() {
       </li>
 
       {/* Card 1 */}
-      <div className="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end container flex justify-end m-10  text-left w-200 h-300">
+      <div className="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end container flex justify-end m-10  text-left w-200 h-300 ml-40 mt-40">
       <div class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50 font-sans text-lime-700">
       ANAMBAS</div>
       <button
@@ -29,7 +29,7 @@ function Pulau() {
       </div>
 
       {/* Card 2 */}
-      <div className="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/4 container flex justify-end m-10 text-center">
+      <div className="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/4 container flex justify-end m-10 text-center mx-0 mt-40">
       <div class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50 font-sans text-lime-700">
       LINGGA </div>
       <button
@@ -43,7 +43,7 @@ function Pulau() {
       </div>
 
       {/* Card 3 */}
-      <div className="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/4 container flex justify-end m-10 text-center">
+      <div className="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/4 container flex justify-end m-10 text-center mx-10 mt-40">
       <div class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50 font-sans text-lime-700">
       KARIMUN </div>
       <button
@@ -57,7 +57,7 @@ function Pulau() {
       </div>
 
       {/* Card 4 */}
-      <div className="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/4 container flex justify-end m-10 text-center">
+      <div className="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/4 container flex justify-end m-10 text-center ml-0 mt-40">
       <div class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50 font-sans text-lime-700">
       BATAM </div>
       <button
@@ -68,6 +68,7 @@ function Pulau() {
           View
         </button>
         {/* Konten Card 4 */}
+    <head></head>
       </div>
     </div>
   );
