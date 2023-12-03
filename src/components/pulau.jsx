22084736a9f47e5d2facf569ad15e54651515a16
StyvenZhuo <151433246+StyvenZhuo@users.noreply.github.com>
2023-12-03 11:49:22 +0700

    Update pulau.jsx

--- src/components/pulau.jsx
+++ src/components/pulau.jsx
@@ -1,44 +1,77 @@
-import React from 'react'
+import React from 'react';
 
-function pulau() {
+function Pulau() {
   return (
-    <div className=''>
-        <div className='text-center text-8xl font-extrabold text-white'>
-        <div class="text-left">Natuna</div>
-        </div>
-    <button class="text-white rounded-full border-2 text-xs font-medium uppercase leading-normal text border-white text-lg ">VIEW</button>
+    <div className='flex'>
+      <li>
+        <h1 className='text-7xl font-serif font-bold text-white ml-20 text-left'>Natuna</h1>
+        <button
+          type="button"
+          className="mt-4 ml-20 inline-block bg-primary bg-transparent hover:bg-blue-500 text-white font-semibold hover:text-white py-2 px-4 border border-white hover:border-transparent rounded-full"
+          data-te-ripple-init
+          data-te-ripple-color="light">
+          View
+        </button>
+      </li>
 
-    <div class="basis-1/3 hover:basis-1/2">
-    <div class="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/6 container flex justify-end m-10 text-center container flex justify-end m-10 text-center ">
-  <div class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50">
-    ANAMBAS
-  </div>
-  <button class="text-green rounded-full border-2 text-xs font-medium uppercase leading-normal text border-lime-500 ">VIEW
-  </button>
-  </div>
+      {/* Card 1 */}
+      <div className="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end container flex justify-end m-10  text-left w-200 h-300">
+      <div class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50 font-sans text-lime-700">
+      ANAMBAS</div>
+      <button
+          type="button"
+          className="mt-20 ml-05 inline-block bg-primary bg-transparent hover:bg-lime-600 text-lime-700 font-semibold hover:text-white py-2 px-4 border border-lime-600 hover:border-lime-500 rounded-full text-sm"
+          data-te-ripple-init
+          data-te-ripple-color="light">
+          View
+        </button>
+        {/* Konten Card 1 */}
+      </div>
 
-<div class="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/6 container flex justify-end m-10 text-center">
-  <div class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50">
-    LINGGA
-  </div>
-  <button class="text-green rounded-full border-2 text-xs font-medium uppercase leading-normal text border-lime-500 ">VIEW</button>
-</div> 
+      {/* Card 2 */}
+      <div className="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/4 container flex justify-end m-10 text-center">
+      <div class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50 font-sans text-lime-700">
+      LINGGA </div>
+      <button
+          type="button"
+          className="mt-20 ml-05 inline-block bg-primary bg-transparent hover:bg-lime-600 text-lime-700 font-semibold hover:text-white py-2 px-4 border border-lime-600 hover:border-lime-500 rounded-full text-sm"
+          data-te-ripple-init
+          data-te-ripple-color="light">
+          View
+        </button>
+        {/* Konten Card 2 */}
+      </div>
 
-<div class="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/6 container flex justify-end m-10 text-center">
-  <div class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50">
-    KARIMUN
-  </div>
-  <button class="text-green rounded-full border-2 text-xs font-medium uppercase leading-normal text border-lime-500 ">VIEW</button>
-</div>
-<div class="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/6 container flex justify-end m-10 text-center">
-  <div class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50">
-    BATAM
-  </div>
-  <button class="text-green rounded-full border-2 text-xs font-medium uppercase leading-normal text border-lime-500 ">VIEW</button>
-</div> </div>
+      {/* Card 3 */}
+      <div className="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/4 container flex justify-end m-10 text-center">
+      <div class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50 font-sans text-lime-700">
+      KARIMUN </div>
+      <button
+          type="button"
+          className="mt-20 ml-05 inline-block bg-primary bg-transparent hover:bg-lime-600 text-lime-700 font-semibold hover:text-white py-2 px-4 border border-lime-600 hover:border-lime-500 rounded-full text-sm"
+          data-te-ripple-init
+          data-te-ripple-color="light">
+          View
+        </button>
+        {/* Konten Card 3 */}
+      </div>
+
+      {/* Card 4 */}
+      <div className="block rounded-lg bg-white p-10 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] green:bg-neutral-700 container flex justify-end w-1/4 container flex justify-end m-10 text-center">
+      <div class="mb-2 text-xl font-medium leading-tight text-neutral-800 green:text-neutral-50 font-sans text-lime-700">
+      BATAM </div>
+      <button
+          type="button"
+          className="mt-20 ml-05 inline-block bg-primary bg-transparent hover:bg-lime-600 text-lime-700 font-semibold hover:text-white py-2 px-4 border border-lime-600 hover:border-lime-500 rounded-full text-sm"
+          data-te-ripple-init
+          data-te-ripple-color="light">
+          View
+        </button>
+        {/* Konten Card 4 */}
+      </div>
     </div>
-
-  )
+  );
 }
 
-export default pulau
+
+export default Pulau;
