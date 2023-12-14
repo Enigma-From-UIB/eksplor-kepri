56c9bfd1025b5ef3dea050201f54600c08854c05
snowyish <90228920+snowyish@users.noreply.github.com>
2023-12-14 21:23:32 +0700

    Update image.js

--- src/assets/image.js
+++ src/assets/image.js
@@ -7,6 +7,22 @@ import tepung from '../files/Kulinerpage/tepung.jpg';
 import Natuna from '../files/pulau/Natuna.png';
 import Anambas from '../files/pulau/Anambas.png';
 import Lingga from '../files/pulau/Lingga.png';
+import Cat from '../files/homepage/cat-1.jpg'
+import Cat2 from '../files/homepage/cat-2.jpg'
+import Cat3 from '../files/homepage/cat-3.jpg'
+import gonggong from '../files/Kulinerpage/gongong.jpg'
+import luti from '../files/Kulinerpage/luti.jpg'
+import tepung from '../files/Kulinerpage/tepung.jpg'
+import gameBG from '../files/game/game-bg.svg'
+import kiddos from '../files/game/kiddos.jpg'
+import mimi from '../files/game/mimi.jpg'
+import unyu from '../files/game/unyu.jpg'
+import wado from '../files/game/wado.jpg'
+import HomeBG1 from '../files/homepage/Header-BG1.svg'
+import HomeBG2 from '../files/homepage/Header-BG2.svg'
+import HomeBG3 from '../files/homepage/Header-BG3.svg'
+import HomeBG4 from '../files/homepage/Header-BG4.svg'
+import HomeBG5 from '../files/homepage/Header-BG5.svg'
 
 
 
@@ -20,6 +36,15 @@ const image = {
     Natuna,
     Anambas,
     Lingga,
-
+    gameBG,
+    kiddos,
+    mimi,
+    unyu,
+    wado,
+    HomeBG1,
+    HomeBG2,
+    HomeBG3,
+    HomeBG4,
+    HomeBG5,
 }
 export default image;
