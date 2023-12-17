64974986110e6bd5e71291c5331cd27fa989a2d1
ZAfifa15 <147030759+ZAfifa15@users.noreply.github.com>
2023-12-17 22:56:09 +0700

    up

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar';
-import Home from './components/Home';
+import Home from './components/home';
 import Pulau from './components/pulau';
-import Wisata from './components/Wisata';
+import Wisata from './components/wisata';
 import Kuliner from './components/kuliner';
-import Budaya from './components/Budaya';
-import Game from './components/Game';
-import Footer from './components/Footer';
+import Budaya from './components/budaya';
+import Game from './components/game';
+import Footer from './components/footer';
 
 function App() {
   return (
@@ -17,11 +17,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="*" element={<Home />} />
-          <Route path="/pulau" element={<Pulau />} />
-          <Route path="/wisata" element={<Wisata />} />
-          <Route path="/kuliner" element={<Kuliner />} />
-          <Route path="/budaya" element={<Budaya />} />
-          <Route path="/game" element={<Game />} />
+          <Route path="/Pulau" element={<Pulau />} />
+          <Route path="/Wisata" element={<Wisata />} />
+          <Route path="/Kuliner" element={<Kuliner />} />
+          <Route path="/Budaya" element={<Budaya />} />
+          <Route path="/Game" element={<Game />} />
         </Routes>
         <Footer />
       </div>
