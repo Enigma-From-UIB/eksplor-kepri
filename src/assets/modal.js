3a22c0cb0af23f1643f2fd8962dbd3a236a8a8cb
snowyish <90228920+snowyish@users.noreply.github.com>
2023-12-20 16:35:25 +0700

    funfact img size shld be same now

--- src/assets/modal.js
+++ src/assets/modal.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Modal = ({ closeModal, children }) => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="fixed inset-0 z-40 flex items-center justify-center bg-black bg-opacity-50">
       <div className="relative bg-white p-8 rounded-lg">
         <button
-          className="absolute top-2 right-2 text-black cursor-pointer lg:text-6xl sm:text-4xl pr-14 pt-10"
+          className="absolute z-100 top-2 right-2 text-black cursor-pointer lg:text-6xl sm:text-4xl pr-14 pt-10"
           onClick={closeModal}
         >
           X
